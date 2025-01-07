c7e8090e7f7ebeacd178883bfb783e1211f5c7a1
ViKtoRg21 <victor._@hotmail.com.br>
2025-01-07 14:20:51 -0300

    Exercicio de logica

---
+++ main.java
@@ -0,0 +1,19 @@
+/**
+ 01) Faça um programa que recebe um número do teclado e calcula o dobro utilizando um
+ método.
+ **/
+import java.util.Scanner;
+
+public class main{
+    public static void main (String[] args) {
+
+    Scanner ler = new Scanner(System.in);
+
+    System.out.println("Digite um número: ");
+    int numero = ler.nextInt();
+
+    int resultado = numero * 2;
+    System.out.println("O numero é" + resultado);
+
+    }
+}
